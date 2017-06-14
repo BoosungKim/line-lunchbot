d9ef89bde3fdc3b9ddf2e45375568afb65ac53e0
busungkim <busungkim@linecorp.com>
2017-06-14 16:22:09 +0900

    Hotfix
    - 영문 이름을 한글로 변경
    - cron 설정 수정

--- src/main/java/com/example/bot/constant/Birthday.java
+++ src/main/java/com/example/bot/constant/Birthday.java
@@ -4,16 +4,16 @@ package com.example.bot.constant;
  * Created by lineplus on 2017. 6. 7..
  */
 public enum Birthday {
-	Junyeong("0607"),
-	Sojeong(""),
-	Jaeho(""),
-	Sangwook(""),
-	Hyeongjun(""),
-	Jonghewk(""),
-	Juyong(""),
-	Busung(""),
-	Jiho(""),
-	Youngjun("");
+	준영("0607"),
+	소정("1226"),
+	재호("0703"),
+	상욱("0516"),
+	형준("0325"),
+	종혁("0715"),
+	주용(""),
+	부성("0214"),
+	지호(""),
+	영준("");
 	private String day;
 	Birthday(String day){
 		this.day = day;
